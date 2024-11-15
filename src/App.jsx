fd50f294e35ab3621de67b6d907830246075449e
lucasr <rossetto.luc@gmail.com>
2024-11-15 18:46:24 +0100

    training on useState on react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Person() {
+  const [person, setPerson] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    age: 100,
+  });
+
+  const handleFirstName = (e) => {
+    let value = e.target.value;
+    setPerson((prevPerson) => ({
+      ...prevPerson,
+      firstName: value,
+    }));
+  };
+  const handleLastName = (e) => {
+    let value = e.target.value;
+    setPerson((prevPerson) => ({
+      ...prevPerson,
+      lastName: value,
+    }));
+  };
+  const handleIncreaseAge = () => {
+    setPerson((person) => ({
+      ...person,
+      age: person.age + 1,
+    }));
+    // we've called setPerson, surely person has updated?
+  };
+
+  // this console.log runs every time the component renders
+  // what do you think this will print?
+  console.log("during render: ", person);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{person.firstName + " " + person.lastName}</h1>
+      <h2>{person.age}</h2>
+      <input
+        type="text"
+        value={person.firstName}
+        onChange={handleFirstName.bind(this)}
+      ></input>
+      <input
+        type="text"
+        value={person.lastName}
+        onChange={handleLastName.bind(this)}
+      ></input>
+      <button onClick={handleIncreaseAge}>Increase age</button>
     </>
-  )
+  );
 }
 
-export default App
+/*function Component() {
+  const [count, setCount] = useState({ inCount: 1 });
+
+  const increaseCount = () => {
+    setCount({ ...count, inCount: count.inCount + 1 });
+  };
+
+  return (
+    <>
+      <h1>{count.inCount}</h1>
+      <button onClick={increaseCount}></button>
+    </>
+  );
+}*/
+
+export default function App() {
+  return (
+    <div>
+      <Person />
+    </div>
+  );
+}
